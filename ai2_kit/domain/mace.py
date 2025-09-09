7cf3224fc447adca14d0c6f74956c44b0824b2a1
Xiliang <xiliang.lian@gmail.com>
2025-09-09 15:07:29 +0200

    modified:   mace.py 	deleted:    macelmps.py

--- ai2_kit/domain/mace.py
+++ ai2_kit/domain/mace.py
@@ -2,9 +2,9 @@ from ai2_kit.core.artifact import Artifact, ArtifactDict
 from ai2_kit.core.script import BashScript, BashStep, BashSteps, BashTemplate, make_gpu_parallel_steps
 from ai2_kit.core.job import gather_jobs
 from ai2_kit.core.log import get_logger
-from ai2_kit.core.util import dict_nested_get, expand_globs, dump_json, list_split, flatten, create_fn
+from ai2_kit.core.util import dict_nested_get, expand_globs, dump_json, list_split, flatten, create_fn, get_yaml
 from ai2_kit.core.pydantic import BaseModel
-from ai2_kit.tool.dpdata import set_fparam, register_data_types
+# from ai2_kit.tool.dpdata import set_fparam, register_data_types
 
 from typing import List, Tuple, Optional
 from dataclasses import dataclass
@@ -17,15 +17,13 @@ import dpdata
 import numpy as np
 
 from .iface import ICllTrainOutput, BaseCllContext, TRAINING_MODE
-from .data import DataFormat, get_data_format
+from .data import DataFormat, get_data_format, write_mace_cumulative_dataset
+from .deepmd import make_deepmd_dataset
 
 from .constant import (
-    DP_CHECKPOINT_FILE,
-    DP_DISP_FILE,
-    DP_PROFILING_FILE,
-    DP_INPUT_FILE,
-    DP_FROZEN_MODEL,
-    DP_ORIGINAL_MODEL,
+    MACE_INPUT_FILE,
+    MACE_FINAL_MODEL, 
+    MACE_LMP_MODEL,
 )
 
 logger = get_logger(__name__)
@@ -105,15 +103,15 @@ class CllMaceInputConfig(BaseModel):
     Ignore non critical errors.
     """
 
-    dp_train_opts: str = ''
+    mace_train_opts: str = ''
     """
-    Extra options for dp train command
+    Extra options for mace train command
     """
 
 
 class CllMaceContextConfig(BaseModel):
     script_template: BashTemplate
-    dp_cmd: str = 'mace_run_train'
+    mace_cmd: str = 'mace_run_train'
     concurrency: int = 5
     multi_gpus_per_job: bool = False
 
@@ -193,29 +191,35 @@ async def cll_mace(input: CllMaceInput, ctx: CllMaceContext):
 
     # for MACE, the cumulative xyz file will be written after each iteration
     # train_systems is a list of strings correspond to the data path
-    
+    all_datasets = input_dataset + init_dataset
+    train_artifacts = [a for a in all_datasets if a.url in train_systems]
+
+    # genertate cumulative dataset in the deepmd dataset dir, we basically keep the original deepmd dataset
+    cumulative_train_file = executor.run_python_fn(make_mace_cumulative_dataset)(
+        dataset_dir=new_dataset_dir, 
+        dataset_collection=[a.to_dict() for a in train_artifacts], 
+        type_map=input.type_map, 
+        extxyzkey = ['ref_energy', 'ref_forces'],
+    )
 
     # make task dirs
-    dp_task_dirs, dw_task_dir = executor.run_python_fn(make_deepmd_task_dirs)(
+    mace_task_dirs = executor.run_python_fn(make_mace_task_dirs)(
         input_template=input.config.input_template,
         model_num=input.config.model_num,
         type_map=input.type_map,
-        train_systems=train_systems,
-        outlier_systems=outlier_systems,
-        validation_systems=validation_systems,
-        outlier_weight=input.config.outlier_weight,
         isolate_outliers=input.config.isolate_outliers,
-        dw_input_template=dw_input_template,
         base_dir=tasks_dir,
+        cumulative_train_file=cumulative_train_file,
         input_modifier_fn=input.config.input_modifier_fn,
     )
 
 
     all_steps: List[BashSteps] = []
+
     # run dp training jobs
 
-    for i, task_dir in enumerate(dp_task_dirs):
-        executor.mkdir(task_dir)
+    for i, task_dir in enumerate(mace_task_dirs):
+
         # FIXME: a temporary workaround to support previous model
         # Here we assume that the model are on the same cluster
         # it will fail if we support multiple cluster in the future
@@ -223,15 +227,16 @@ async def cll_mace(input: CllMaceInput, ctx: CllMaceContext):
         if input.config.init_from_previous and input.previous:
             j = i % len(input.previous)
             previous_model = os.path.join(os.path.dirname(input.previous[j].url),
-                                          DP_ORIGINAL_MODEL)
+                                          MACE_FINAL_MODEL)
 
-        steps = _build_deepmd_steps(
-            dp_cmd=ctx.config.dp_cmd,
+        steps = _build_mace_steps(
+            mace_cmd=ctx.config.mace_cmd,
             compress_model=input.config.compress_model,
             cwd=task_dir,
             pretrained_model=input.config.pretrained_model,
             previous_model=previous_model,
-            dp_train_opts=input.config.dp_train_opts,
+            mace_train_opts=input.config.mace_train_opts,
+            model_name='mace_model',  # Ensure consistent model name
         )
         all_steps.append(steps)
 
@@ -258,13 +263,34 @@ async def cll_mace(input: CllMaceInput, ctx: CllMaceContext):
 
     await gather_jobs(jobs, max_tries=2)
 
-    logger.info(f'All models are trained, output dirs: {dp_task_dirs}')
-    return GenericDeepmdOutput(
+    logger.info(f'All models are trained, output dirs: {mace_task_dirs}')
+
+    # collect trained models (both .model and -mliap_lammps.pt versions)
+    models = []
+    for i, task_dir in enumerate(mace_task_dirs):
+        
+        # MACE creates multiple model files:
+        # 1. {name}.model - main MACE model
+        # 2. {name}.model-mliap_lammps.pt - LAMMPS-optimized model (created by mace_create_lammps_model)
+        
+        # Choose appropriate model file based on compression setting
+        if input.config.compress_model:
+            final_model_file = os.path.join(task_dir, MACE_LMP_MODEL)
+        else:
+            final_model_file = os.path.join(task_dir, MACE_FINAL_MODEL)
+        
+        models.append(Artifact.of(
+            url=final_model_file,
+            format=DataFormat.MACE_MODEL,
+            attrs={
+                'logs_dir': os.path.join(task_dir, 'logs'),
+                'checkpoints_dir': os.path.join(task_dir, 'checkpoints'),
+            }
+        ))
+
+    return GenericMaceOutput(
         dataset=input_dataset.copy(),
-        models=[Artifact.of(
-            url=os.path.join(url, DP_FROZEN_MODEL),
-            format=DataFormat.DEEPMD_MODEL,
-        ) for url in dp_task_dirs]
+        models=models
     )
 
 def _classify_dataset(dataset: List[Artifact]):
@@ -294,164 +320,108 @@ def _classify_dataset(dataset: List[Artifact]):
     return _unique(train_systems), _unique(outlier_systems), _unique(validation_systems)
 
 
-def _build_deepmd_steps(dp_cmd: str,
+def _build_mace_steps(mace_cmd: str,
                         compress_model: bool,
                         cwd: str,
                         previous_model: Optional[str] = None,
                         pretrained_model: Optional[str] = None,
-                        dp_train_opts: str = '',
+                        mace_train_opts: str = '',
+                        model_name: str = 'mace_model',
                         ):
     steps = []
-    dp_train_cmd = f'{dp_cmd} train {dp_train_opts} {DP_INPUT_FILE}'
-    if previous_model:
-        dp_train_cmd = f'{dp_train_cmd} -f {previous_model}'
-    if pretrained_model:
-        dp_train_cmd = f'{dp_train_cmd} --finetune {pretrained_model}'
+    mace_train_cmd = f'{mace_cmd} --config {MACE_INPUT_FILE} {mace_train_opts}'
 
-    dp_train_cmd_restart = f'if [ ! -f model.ckpt.index ]; then {dp_train_cmd}; else {dp_cmd} train {DP_INPUT_FILE} --restart model.ckpt; fi'
+    # MACE restart logic: check for checkpoint files and restart accordingly
+    if previous_model:
+        mace_train_cmd = f'{mace_train_cmd} --restart-training {previous_model}'
+    if pretrained_model:
+        mace_train_cmd = f'{mace_train_cmd} --foundation-model {pretrained_model}'
+    
+    # MACE checkpoint restart logic
+    mace_train_cmd_restart = f'if [ ! -f {model_name}.model ]; then {mace_train_cmd}; else echo "Model already trained, skipping..."; fi'
 
     steps.append(
-        BashStep(cmd=dp_train_cmd_restart, cwd=cwd, checkpoint='dp-train')  # type: ignore
+        BashStep(cmd=mace_train_cmd_restart, cwd=cwd, checkpoint='mace-train')  # type: ignore
     )
+
     if compress_model:
-        steps.append(BashStep(cmd=[dp_cmd, 'freeze', '-o', DP_ORIGINAL_MODEL, '&&',
-                                   dp_cmd, 'compress', '-i', DP_ORIGINAL_MODEL, '-o', DP_FROZEN_MODEL],
-                              cwd=cwd))
-    else:
-        # FIXME: a temporary workaround to support previous model
-        steps.append(BashStep(cmd=[dp_cmd, 'freeze', '-o', DP_ORIGINAL_MODEL, '&&',
-                                   'cp', DP_ORIGINAL_MODEL, DP_FROZEN_MODEL],
+        steps.append(BashStep(cmd=[f'mace_create_lammps_model', f'{model_name}.model', '--format=mliap'],
                               cwd=cwd))
     return steps
 
 
-def make_deepmd_task_dirs(input_template: dict,
+def make_mace_task_dirs(input_template: dict,
                           model_num: int,
                           type_map: List[str],
-                          train_systems: List[str],
-                          outlier_systems: List[str],
-                          validation_systems: List[str],
                           isolate_outliers: bool,
-                          outlier_weight: float,
-                          dw_input_template: Optional[dict],
                           base_dir: str,
+                          cumulative_train_file: str,
                           input_modifier_fn: Optional[str],
                           ):
 
     input_modifier = create_fn(input_modifier_fn, 'input_modifier_fn') if input_modifier_fn else lambda x: x
 
-    dp_task_dirs = [os.path.join(base_dir, f'{i:03d}')  for i in range(model_num)]
-    for task_dir in dp_task_dirs:
+    mace_task_dirs = [os.path.join(base_dir, f'{i:03d}')  for i in range(model_num)]
+    for task_dir in mace_task_dirs:
         os.makedirs(task_dir, exist_ok=True)
-        dp_input = make_deepmd_input(
+        mace_input = make_mace_input(
             input_template=input_template,
             type_map=type_map,
-            train_systems=train_systems,
-            outlier_systems=outlier_systems,
-            validation_systems=validation_systems,
             isolate_outliers=isolate_outliers,
-            outlier_weight=outlier_weight,
+            train_file=cumulative_train_file,
         )
 
-        # If dw_model is set in dp_input,
-        # it will create a softlink named dw_model.pb to the current dir,
-        # and then modify the value in dw_model to dw_model.pb.
-        dw_model = dict_nested_get(dp_input, ['model', 'modifier', 'model_name'], None)
-        if dw_model is not None:
-            # Create a soft link named 'dw_model.pb' to the file specified by dw_model
-            link_target = os.path.join(task_dir, 'dw_model.pb')
-            os.system(f'ln -sf {dw_model} {link_target}')
-            # Modify the value in dw_model to 'dw_model.pb'
-            dp_input['model']['modifier']['model_name'] = 'dw_model.pb'
+        mace_input = input_modifier(mace_input)
+        
+        # Convert config dict to YAML string format
+        # Using StringIO because ruamel.yaml.dump() requires a file-like object
+        yaml = get_yaml()
+        from io import StringIO
+        output = StringIO()
+        yaml.dump(mace_input, output)
+        mace_input_yaml = output.getvalue()
+        
+        mace_input_path = os.path.join(task_dir, MACE_INPUT_FILE)
+        with open(mace_input_path, 'w') as f:
+            f.write(mace_input_yaml)
 
-        dp_input = input_modifier(dp_input)
-        dp_input_path = os.path.join(task_dir, DP_INPUT_FILE)
-        dump_json(dp_input, dp_input_path)
-
-    dw_task_dir = None
-    if dw_input_template:
-        dw_task_dir = os.path.join(base_dir, 'train_dw')
-        os.makedirs(dw_task_dir, exist_ok=True)
-        dw_input = make_deepmd_input(
-            input_template=dw_input_template,
-            type_map=type_map,
-            train_systems=train_systems,
-            outlier_systems=outlier_systems,
-            validation_systems=validation_systems,
-            isolate_outliers=isolate_outliers,
-            outlier_weight=outlier_weight,
-        )
-        dw_input_path = os.path.join(dw_task_dir, DP_INPUT_FILE)
-        dump_json(dw_input, dw_input_path)
-
-    return dp_task_dirs, dw_task_dir
+    return mace_task_dirs
 
 
-def make_deepmd_input(input_template: dict,
+def make_mace_input(input_template: dict,
                       type_map: List[str],
-                      train_systems: List[str],
-                      outlier_systems: List[str],
-                      validation_systems: List[str],
                       isolate_outliers: bool,
-                      outlier_weight: float,
+                      train_file: str,
                       ):
     # create dp train input file
-    # ref: https://github.com/deepmodeling/dpgen2/blob/master/examples/ch4/param_CH4_deepmd-kit-2.1.1.json
-    # ref: https://github.com/deepmodeling/dpgen2/blob/master/dpgen2/op/prep_dp_train.py
-    # ref: https://github.com/deepmodeling/dpgen2/blob/master/dpgen2/op/run_dp_train.py
+    """
+    generate the input file, dynamic variables needs modification
+    """
 
     def _random_seed():
         return random.randrange(sys.maxsize) % (1 << 32)
 
-    dp_input = copy.deepcopy(input_template)
-    training: dict = dp_input['training']
+    mace_input = copy.deepcopy(input_template)
 
-    # set output files
-    training['disp_file'] = DP_DISP_FILE
-    training['save_ckpt'] = DP_CHECKPOINT_FILE
-    training['profiling_file'] = DP_PROFILING_FILE
+    mace_input['seed'] = _random_seed()
+    mace_input['train_file'] = train_file
 
-    # set random seed
-    discriptor = dp_input['model']['descriptor']
-    if discriptor['type'] == 'hybrid':
-        for d in discriptor['list']:
-            d['seed'] = _random_seed()
-    else:
-        discriptor['seed'] = _random_seed()
-    dp_input['model']['fitting_net']['seed'] = _random_seed()
-    dp_input['training']['seed'] = _random_seed()
+    mace_input['name'] = f"mace_model"
+    
+    return mace_input
 
-    auto_prob_str = "prob_sys_size"
-    if isolate_outliers and len(outlier_systems) > 0:
-        # if isolate_outlier is enabled, then we need to set different weight for outlier data
-        # e.g: prob_sys_size;0:57:0.9997;57:60:0.0003
-        auto_prob_str += f';0:{len(train_systems)}:{1-outlier_weight}'
-        auto_prob_str += f';{len(train_systems)}:{len(train_systems)+len(outlier_systems)}:{outlier_weight}'
 
-    # v2 training data
-    training_data = training.get('training_data', {})
-    training_data = {
-        'set_prefix': 'set',
-        'batch_size': 'auto',
-        'sys_probs': None,
-        ** training_data,
-        'systems': train_systems + outlier_systems,
-        'auto_prob_style': auto_prob_str,
-    }
-    training['training_data'] = training_data
+def make_mace_cumulative_dataset(
+        dataset_dir: str, 
+        dataset_collection: List[ArtifactDict],
+        type_map: List[str],
+        extxyzkey: List[str] = ['ref_energy', 'ref_forces'],
+):
+    """create mace cumulative dataset since this is mace specific"""
+    os.makedirs(dataset_dir, exist_ok=True)
+    train_file = os.path.join(dataset_dir, 'train.xyz')
 
-    # ignore validation section if no data is provided, or else dp will throw error
-    # OSError: [Errno cannot find valid a data system] Please check your setting for data systems
-    if len(validation_systems) > 0:
-        validation_data = {
-            'systems': validation_systems,
-            'set_prefix': training_data['set_prefix'],
-            'batch_size': training_data['batch_size'],
-        }
-        training['validation_data'] = validation_data
-
-    # other params
-    dp_input['model']['type_map'] = type_map
-
-    return dp_input
-
+    # define a function to write the cumulative file in data.py
+    write_mace_cumulative_dataset(train_file, dataset_collection, type_map, extxyzkey)
+    logger.info(f'Created cumulative dataset: {train_file}')
+    return train_file
